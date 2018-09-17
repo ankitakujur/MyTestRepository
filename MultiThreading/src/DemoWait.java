8ddbddc67ac7f7a17072db4c7a3f373dc4045608
ankitakujur <kujurankita52@gmail.com>
2018-09-17 12:05:17 +0530

    what

--- MultiThreading/src/DemoWait.java
+++ MultiThreading/src/DemoWait.java
@@ -3,8 +3,19 @@ public class DemoWait {
 	public static void main(String[] args) {
 		DemoNotify t1=new DemoNotify();
 		t1.start();
-		t1.wait();
+		try
+		{
+			
+		synchronized (t1) {
+			
 		
+		t1.wait();
+		System.out.println("sum="+t1.sum);
+		}
+		}catch(InterruptedException e)
+		{
+			e.printStackTrace();
+		}
 		System.out.println("Sum is "+t1.sum);
 		
 	}
